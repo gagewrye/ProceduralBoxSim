46ce1d63645bdd3fb0f12a2d637d059450155ec6
gagewrye <95107220+gagewrye@users.noreply.github.com>
2024-03-31 08:18:48 +0200

    argparser

--- generate_map.py
+++ generate_map.py
@@ -1,12 +1,30 @@
+from argparse import ArgumentParser, Namespace
 import random
 import MinimumSpanningTree
 
+def parse_args() -> Namespace:
+    arg_parser = ArgumentParser("Train command classification networks.")
 
+    arg_parser.add_argument("num_rooms", help="Number of rooms on map")
+    arg_parser.add_argument("--room_seed", default=47, help="Determines what the map looks like")
+    arg_parser.add_argument("--map_size_x", default=100, help="X-axis size")
+    arg_parser.add_argument("--map_size_y", default=100, help="Y-axis size")
+    arg_parser.add_argument("--max_room_size", default=1, help="How large the rooms can be") 
+    arg_parser.add_argument("--num_cycles",
+                            default=2,
+                            help="cycles will add complexity and loops to the map, creating more challenging exploration")
+    return arg_parser.parse_args()
 
 
 
 def __main__():
-    rand = random.seed(47)
-    mst = MinimumSpanningTree.MST(5, 100, 100, 1, rand)
-    mst.add_cycles(2)
+    args = parse_args()
+    rand = random.seed(args.room_seed)
+    mst = MinimumSpanningTree.MST(args.num_rooms, args.map_size_x, args.map_size_y, args.max_room_size, rand)
+    mst.add_cycles(args.num_cycles)
+    # Build Rooms
+    # Build Hallways
     return map
+
+if __name__ == "__main__":
+    main()
